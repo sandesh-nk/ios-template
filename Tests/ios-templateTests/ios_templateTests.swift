5793e5f2f7cbd2f463ea91825164e22b1c3055e8
Sandesh Naik <sandesh@wednesday.is>
2021-11-25 21:25:58 +0530

    Added test failure

--- Tests/ios-templateTests/ios_templateTests.swift
+++ Tests/ios-templateTests/ios_templateTests.swift
@@ -6,6 +6,6 @@ final class IOSTemplateTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ios_template().text, "Hello, World!")
+        XCTAssertEqual(IOSTemplate().text, "Hello, World!")
     }
 }
